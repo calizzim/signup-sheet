547133afa4277155cb26623ab7b2519a6c4e166e
Michael Calizzi <calizzim@gmail.com>
2021-06-14 16:17:46 -0500

    got the get request to the backend working

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  constructor(private http:HttpClient) {}
+  ngOnInit() {this.getSheet()}
+
+  getSheet() {
+    this.http.get('http://localhost:3000').subscribe(
+      result => {
+        // console.log(result)
+        // console.log(result[0].firstName)
+        for(let obj in result){
+          console.log(`first name: ${result[obj].firstName}\nlast name: ${result[obj].lastName}`)
+        }
+      },
+      error => {
+        console.log("unsuccessful");
+      }
+    );
+  }
 }
