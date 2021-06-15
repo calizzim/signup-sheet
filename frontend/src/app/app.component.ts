3b4732ad1ae6d033c3996c5d64028dfa030d0c8a
Michael Calizzi <calizzim@gmail.com>
2021-06-14 19:00:02 -0500

    completed signup sheet and connected to remote database

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,21 +8,47 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit{
   title = 'frontend';
+  names:[any];
+  url = 'http://localhost:3000/'
   constructor(private http:HttpClient) {}
   ngOnInit() {this.getSheet()}
 
   getSheet() {
-    this.http.get('http://localhost:3000').subscribe(
-      result => {
-        // console.log(result)
-        // console.log(result[0].firstName)
-        for(let obj in result){
-          console.log(`first name: ${result[obj].firstName}\nlast name: ${result[obj].lastName}`)
-        }
+    this.http.get(this.url).subscribe(
+      (result:Response) => {
+        this.names = result.body
       },
       error => {
         console.log("unsuccessful");
       }
     );
   }
+
+  firstName = '';
+  lastName = '';
+  
+  addName() {
+    this.http.post(this.url, {
+      firstName: this.firstName,
+      lastName: this.lastName
+    }).subscribe(
+      result => { this.getSheet() },
+      error => { console.log(error) }
+    );
+    this.firstName = '';
+    this.lastName = '';
+  }
+
+  
+  deleteName(id){
+    this.http.delete(this.url+id).subscribe(
+      result => { this.getSheet() },
+      error => { console.log(error) }
+    );
+  }
+}
+
+
+interface Response {
+  body: [any]
 }
